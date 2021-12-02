32340c3b57ec9dd39add535ba7e5101c97cbc388
lyr <lyr>
2021-12-02 10:21:13 +0800

    修改了Result类的错误

--- src/main/java/com/sdyb/mp/util/Result.java
+++ src/main/java/com/sdyb/mp/util/Result.java
@@ -1,30 +1,15 @@
 package com.sdyb.mp.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.util.HashMap;
 
 /**
  * 结果返回类
  */
 @Data
-public class Result extends HashMap<String, Object> {
-
-    /** 0-成功 1-失败（非0则失败，因此传递其他数字也是可以的，比如404,403） */
-    private int code=0;
-
-    /** 返回的说明信息 */
-    private String msg="";
-
-    /** data是Object修饰的，因此能放进任何形式的东西 */
-    public Object data="";
-
-    public Result(){
-        super();
-        this.put("code",code);
-        this.put("msg",msg);
-        this.put("data",data);
-    }
+@AllArgsConstructor
+public class Result extends HashMap<String, Object>  {
 
     @Override
     public Result put(String key, Object value){
@@ -32,19 +17,16 @@ public class Result extends HashMap<String, Object> {
         return this;
     }
 
-    public Result putData(Object data) {
+    public void putData(Object data) {
         this.put("data", data);
-        return this;
     }
 
-    public Result putMsg(String msg) {
-        this.put("msg", msg);
-        return this;
+    public void putMsg(String msg) {
+        this.put("msg",msg);
     }
 
-    public Result putCode(int code) {
-        this.put("code", code);
-        return this;
+    public void putCode(int code) {
+        this.put("code",code);
     }
 
 }
